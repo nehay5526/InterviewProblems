869e8267f7f1ca46d26e971c359cd0f58549b156
nehay5526 <nehay5526@gmail.com>
2024-04-26 00:04:46 +0530

    Initial commit

---
+++ src/test/java/com/interview/interviewproblems/InterviewProblemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.interviewproblems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewProblemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
